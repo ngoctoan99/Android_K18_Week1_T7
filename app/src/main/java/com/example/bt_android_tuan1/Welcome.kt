ad898954d303be12c2eab83f8888950a5bd254b0
USER <nguyenngoctoan2110@gmail.com>
2022-03-11 15:57:30 +0700

    Android Week 1

---
+++ app/src/main/java/com/example/bt_android_tuan1/Welcome.kt
@@ -0,0 +1,11 @@
+package com.example.bt_android_tuan1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Welcome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
